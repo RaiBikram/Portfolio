44c0e0493766d3a783bd09ea0bb2486f21314267
Bikram Rai <bikramdevlopment@gmail.com>
2025-01-02 11:22:52 +0545

    updae home path

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 <div className="flex items-center justify-center flex-wrap mt-6 ">
 <nav className=" flex flex-wrap  items-center justify-center gap-10 font-extrabolds text-xl bg-green-800 rounded-2xl lg:w-1/3 h-16 p-2 sm:2/3 md:1/3">
 
-<NavLink to={"/home"} className={"hover:bg-slate-800 hover:rounded-lg p-2"}>Home</NavLink>
+<NavLink to={"/"} className={"hover:bg-slate-800 hover:rounded-lg p-2"}>Home</NavLink>
 <NavLink to={"/about"} className={"hover:bg-slate-800 hover:rounded-lg p-2"}>About</NavLink>
 <NavLink to={"/projects"} className={"hover:bg-slate-800 hover:rounded-lg p-2"}>Projects</NavLink>
 
