a04bb150d99a10bacf3b48df2197f54514148c98
Bikram Rai <bikramdevlopment@gmail.com>
2025-01-02 11:40:41 +0545

    update responsiveness

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,29 +3,32 @@ import React from "react";
 import { ABOUT_TEXT } from "../utils/index.utils";
 import Technologies from "./Technologies";
 import profilePic from "../media/bikram.jpeg";
+
 export default function AboutSection() {
   return (
-    <div className="border-b border-neutral-900 pb-4 justify-center  flex flex-wrap">
-      <div className=" justify-center items-center">
-        <div className="my-2 max-w-5xl  py-24 text-red-300 w-full flex flex-wrap justify-between">
-          <p className="w-2/3">
-            {" "}
-            <h3 className="justify-start flex lg:justify-start font-semibold text-4xl pb-6">
-              Hey! I'm Bikram Rai and I'm a full stack engineer.
-            </h3>
-            {ABOUT_TEXT}
-          </p>
-          <div className="flex justify-between">
-            <img
-              className="rounded-full "
-              src={profilePic}
-              alt="Image Not Found"
-              width={300}
-              height={150}
-            />
-          </div>
+    <div className="border-b border-neutral-900 pb-8 flex flex-col items-center">
+      <div className="max-w-5xl w-full flex flex-col lg:flex-row items-center lg:items-start lg:justify-between py-16 px-6">
+        {/* Text Section */}
+        <div className="lg:w-2/3 w-full text-center lg:text-left">
+          <h3 className="font-semibold text-4xl pb-6 text-red-300">
+            Hey! I'm Bikram Rai, and I'm a full-stack engineer.
+          </h3>
+          <p className="text-neutral-300 text-base leading-relaxed">{ABOUT_TEXT}</p>
+        </div>
+
+        {/* Profile Picture */}
+        <div className="mt-8 lg:mt-0 flex justify-center lg:justify-end">
+          <img
+            className="rounded-full shadow-lg"
+            src={profilePic}
+            alt="Profile Picture"
+            width={200}
+            height={200}
+          />
         </div>
       </div>
+
+      {/* Technologies Section */}
       <div className="w-full">
         <Technologies />
       </div>
