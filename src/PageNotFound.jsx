1fd4977e5b25d7b1f8d1f7c2188a72fd29e8e285
Bikram Rai <bikramdevlopment@gmail.com>
2025-01-04 21:22:56 +0545

    adding page not found

--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
-import Layout from '../components/layout/Layout'
-import { Link } from 'react-router-dom'
+import React from "react";
+
+import { Link } from "react-router-dom";
 
 export default function PageNotFound() {
   return (
-    <Layout>
-
-<div className="notFound">
-    <h1>404</h1>
-    <h2>OOps ! Page Not Found </h2>
-    <Link to="/">Go Back Home</Link>
-</div>
-    </Layout>
-    
-  )
+    <div className="flex flex-wrap text-center items-center justify-center max-h-max m-20">
+      <h1 className="w-full font-extrabold text-6xl m-10 text-orange-600">404</h1>
+      <h2 className=" w-full text-4xl m-5 text-red-900 font-semibold">OOps ! Page Not Found </h2>
+      <Link to="/" className="text-3xl m-5 text-green-600 font-bold">Go Back Home</Link>
+    </div>
+  );
 }
