3d368dc813a53a2d0777a36ff25c2f336774ed93
Bikram Rai <175561381+ItsBikramRai@users.noreply.github.com>
2025-02-19 01:25:50 +0545

    adding AI Health Assistant Project

--- src/utils/index.utils.js
+++ src/utils/index.utils.js
@@ -2,10 +2,11 @@ import kinmel from "../media/kinmel.png";
 import OAuthGoogle from "../media/oauth.png";
 import portfolio from "../media/portfolio-website.png";
 import weather_widget from "../media/weather-widget.png";
+import  AI_Health_Assistant from "../media/ai-health-asistant.png"
 
 export const HERO_CONTENT = `Crafting efficient and user-friendly web applications with a passion for full stack development.`;
 
-export const ABOUT_TEXT = `I'm Bikram Rai, an aspiring Full Stack Developer passionate about crafting dynamic and responsive web applications. With a strong foundation in both front-end and back-end technologies, including JavaScript, Node.js, MongoDB, React, HTML, CSS, Bootstrap, and Tailwind CSS. I'm eager to bring my skills to the professional world and contribute to innovative projects. Committed to continuous learning and excited to collaborate with experienced teams, I'm ready to build impactful solutions and explore new challenges. Let's connect and discover how we can work together!.`;
+export const ABOUT_TEXT = `I’m Bikram Rai, a MERN stack developer with experience in MongoDB, Express, React, Node.js, and Next.js. Passionate about learning and building efficient, scalable web applications, I’m always eager to explore new technologies and create innovative solutions. Let’s connect and build something impactful!`;
 
 // export const EXPERIENCES = [
 //   {
@@ -35,30 +36,45 @@ export const ABOUT_TEXT = `I'm Bikram Rai, an aspiring Full Stack Developer pass
 //     company: "Paypal",
 //     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
 //     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-    
+
 //   },
 // ];
 
 export const PROJECTS = [
-  
+  {
+    title: "AI Health Assistant",
+    image: AI_Health_Assistant,
+    description:
+      "AI Health Assistant: An AI-powered system for analyzing medical reports, answering queries, and providing insights. Built with Gemini AI, LangChain, RAG, and Pinecone to offer real-time responses and relevant data retrieval for healthcare professionals and patients.",
+    technologies: ["Next.js", "Tailwind"],
+    link: "https://aihealthassistant.vercel.app",
+    github: "https://github.com/ItsBikramRai/AI_Health_Assistant",
+  },
   {
     title: "Kinmel Website",
     image: kinmel,
     description:
       "Kinmel is an e-commerce website built for the Nepal market. It allows users to browse products, filter by category and price, and add items to their cart. This project was developed as part of a portfolio to demonstrate full-stack development skills, focusing on a seamless shopping experience.",
-      
+
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB", "Bootstrap"],
-    link:"https://kinmel.onrender.com",
-    github:"https://github.com/ItsBikramRai/Kinmel"
+    link: "https://kinmel.onrender.com",
+    github: "https://github.com/ItsBikramRai/Kinmel",
   },
   {
     title: "AuthPlus: Google and Email-Based Authentication",
     image: OAuthGoogle,
-    description: 
+    description:
       "AuthPlus is a robust authentication platform designed to manage user login, signup, password reset, and email verification. This platform supports both **Google OAuth** and **manual authentication** methods, giving users the flexibility to log in using their Google account or with their email and password. With seamless integration of **Passport.js**, users can securely sign up, verify their email, and reset forgotten passwords with ease. Whether it's for a web application or service, AuthPlus provides a reliable, secure, and customizable authentication solution.",
-    technologies: ["Node.js", "Express", "MongoDB", "Passport.js", "Tailwind", "OAuth 2.0"],
-    link:"https://google-oauth-blush.vercel.app",
-    github:"https://github.com/ItsBikramRai/googleAuth"
+    technologies: [
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Passport.js",
+      "Tailwind",
+      "OAuth 2.0",
+    ],
+    link: "https://google-oauth-blush.vercel.app",
+    github: "https://github.com/ItsBikramRai/googleAuth",
   },
   // {
   //   title: "Task Management App",
@@ -73,23 +89,22 @@ export const PROJECTS = [
     description:
       "A personal portfolio website showcasing projects, skills, and contact information. This website serves as a platform to highlight my work and demonstrate my abilities as a web developer.",
     technologies: ["HTML", "CSS", "React", "Tailwind"],
-    link:"https://bikram-rai.com.np",
-    github:"https://github.com/ItsBikramRai/Portfolio"
-    
+    link: "https://bikram-rai.com.np",
+    github: "https://github.com/ItsBikramRai/Portfolio",
   },
 
   {
     title: "Weather Widget",
-    description: "Weather Widget is a simple and responsive weather application built using React. It allows users to search for real-time weather data of any location using an external weather API.",
+    description:
+      "Weather Widget is a simple and responsive weather application built using React. It allows users to search for real-time weather data of any location using an external weather API.",
     image: weather_widget,
     technologies: ["HTML", "CSS", "React"],
-    link:"https://weather-widget-zeta-steel.vercel.app",
-    github:"https://github.com/ItsBikramRai/weather-widget"
+    link: "https://weather-widget-zeta-steel.vercel.app",
+    github: "https://github.com/ItsBikramRai/weather-widget",
   },
 ];
 
-
-//contact 
+//contact
 export const CONTACT = {
   address: "Kathmandu, Nepal ",
   // phoneNo: "+9779863963772",
