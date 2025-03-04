7a41d0e1921491c74b4fcb4981d582158b6ae632
Bikram Rai <175561381+ItsBikramRai@users.noreply.github.com>
2025-03-04 09:49:39 +0545

    added smoothscroller and links

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -3,45 +3,71 @@ import { CONTACT } from "../utils/index.utils";
 import { FaSquareGithub } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-
+import { CiMail } from "react-icons/ci";
+import { IoMdMail } from "react-icons/io";
 
 export default function Contact() {
   const currentYear = new Date().getFullYear();
   return (
     <div className=" py-16">
-      {/* <h1 className="my-6 text-center text-4xl">Get in Touch </h1> */}
+      {/* <h1 className="my-6 text-center text-2xl">Get in Touch </h1> */}
       <div className="text-center tracking-tighter">
-        {/* <p className="my-4 ">{CONTACT.address}</p>
-        <p className="my-4 ">{CONTACT.phoneNo}</p> */}
-
-        {/* <span>
-          <div style={{ fontSize: "13px" }}>
-            Designed & Developed by: &nbsp;
-            <a style={{ color: "#4770b3" }} href="https://bikram-rai.com.np">
-              Bikram Rai
-            </a>
-          </div>
-        </span> */}
-
         <div
           className="m-5 flex items-center justify-center gap-5 text-2xl pr-30
       "
         >
-          <a href="https://www.linkedin.com/in/raibikraminfo/">
+          <a
+            href="https://www.linkedin.com/in/raibikraminfo/"
+            onClick={(e) => {
+              e.preventDefault();
+              window.open(
+                "https://www.linkedin.com/in/raibikraminfo",
+                "_blank"
+              );
+            }}
+          >
             <FaLinkedin />
           </a>
 
-          <a href="https://github.com/RaiBikram">
+          <a
+            href="https://github.com/RaiBikram"
+            onClick={(e) => {
+              e.preventDefault();
+              window.open("https://github.com/RaiBikram", "_blank");
+            }}
+          >
             <FaSquareGithub />
           </a>
-          <a href="https://x.com/RaiBkram">
+          <a
+            href="https://x.com/RaiBkram"
+            onClick={(e) => {
+              e.preventDefault();
+              window.open("https://x.com/RaiBkram", "_blank");
+            }}
+          >
             <FaSquareXTwitter />
           </a>
         </div>
-        <p className="mb-2">{CONTACT.email}</p>
+        <a
+          href={`mailto:${CONTACT.email}`}
+          onClick={(e) => {
+            e.preventDefault();
+            window.open(`mailto:${CONTACT.email}`, "_blank");
+          }}
+        >
+          <p className="mb-2">{CONTACT.email}</p>
+        </a>
         <p className="font-semibold">
           &copy; {currentYear},{" "}
-          <a href="https://bikram-rai.com.np">Bikram Rai</a>
+          <a
+            href="https://bikram-rai.com.np"
+            onClick={(e) => {
+              e.preventDefault();
+              window.open("https://bikram-rai.com.np", "_blank");
+            }}
+          >
+            Bikram Rai
+          </a>
         </p>
       </div>
     </div>
