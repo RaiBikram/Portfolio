0e9320014151aba725eab7192e69822132d1133e
Bikram Rai <175561381+ItsBikramRai@users.noreply.github.com>
2025-03-13 20:10:11 +0545

    fixes nav

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="pt-6 w-full flex justify-center px-4">
-      <nav className="w-full md:w-1/2 lg:w-1/2 max-w-xs flex items-center justify-evenly bg-green-900 h-12 rounded-full shadow-md text-xs sm:text-sm font-extrabold">
+      <nav className="w-full md:w-1/2 lg:w-1/2 max-w-xs flex items-center justify-evenly bg-green-900 h-12 rounded-full shadow-md text-base sm:text-base font-semibold">
         <NavLink
           to="/"
           className={({ isActive }) =>
